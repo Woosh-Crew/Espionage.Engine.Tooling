172004cb8c6203193249abff507185de9b8dba22
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-04-19 21:02:37 +1000

    Add Resource Viewer Tool

---
+++ Runtime/Tools/Windows/ResourceViewer.cs
@@ -0,0 +1,19 @@
+﻿using Espionage.Engine.Resources;
+using ImGuiNET;
+
+namespace Espionage.Engine.Tools
+{
+	public class ResourceViewer : Window
+	{
+		public override void OnLayout()
+		{
+			foreach ( var resource in Resource.Database )
+			{
+				if ( ImGui.Selectable( $"{resource.Identifier}" ) )
+				{
+					Service.Selection = resource;
+				}
+			}
+		}
+	}
+}
